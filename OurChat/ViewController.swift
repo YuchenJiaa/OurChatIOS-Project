0a27f87018dcb14ad380a5adcbe42318d37feaf6
YuchenJiaa <113528470+YuchenJiaa@users.noreply.github.com>
2023-04-26 07:39:34 -0400

    Initial Commit

---
+++ OurChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OurChat
+//
+//  Created by 贾贾 on 2023/4/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
